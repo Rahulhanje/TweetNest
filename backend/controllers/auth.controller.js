8ef73e913f567279b4877fa7fd099e5bf8a9341f
Rahul <rahulhanje0.7@gmail.com>
2024-12-23 01:00:30 +0530

    Database is setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,14 @@
 export const signup = (req, res) => {
   res.json({message:"signup route hit"});
 };
+
+
+export const login = (req, res) => {
+    res.json({message:"login route hit"});
+  };
+
+
+export const logout = (req, res) => {
+    res.json({message:"logout route hit"});
+  };
+    
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
+import connectDB from "./db/connect.js";
+
+dotenv.config();
+
 const app = express();
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
+  connectDB();
 });
