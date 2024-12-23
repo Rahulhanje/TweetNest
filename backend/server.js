7e1e466954fc92596b2a4d5be1e10bc436b17a17
Rahul <rahulhanje0.7@gmail.com>
2024-12-23 22:02:56 +0530

    developed user routes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
-
-import authRoutes from "./routes/auth.routes.js";
+import {v2 as cloudinary } from "cloudinary";
 import connectDB from "./db/connect.js";
 import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
