cc3ac2c15660b7e943df4d3bc166b068d18656ce
Rahul <rahulhanje0.7@gmail.com>
2024-12-29 12:39:09 +0530

    502 gatway error resolve try3

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import {v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import connectDB from "./db/connect.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
@@ -9,7 +9,6 @@ import postRoutes from "./routes/posts.routes.js";
 import notificationRoutes from "./routes/notification.routes.js";
 import cors from "cors";
 import path from "path";
-
 
 dotenv.config();
 cloudinary.config({
@@ -22,35 +21,30 @@ const app = express();
 
 const __dirname = path.resolve();
 
-
-
-app.use(express.json({limit:"50mb"}));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
-
-
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notification", notificationRoutes);
 
-
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
-server.keepAliveTimeout = 120000; // 120 seconds
-server.headersTimeout = 120000;
-
-app.listen(process.env.PORT || 10000, () => {
+const server = app.listen(process.env.PORT || 10000, () => {
   console.log("Server is running on port 3000");
   connectDB();
 });
+
+// Now you can set the timeouts on the server object
+server.keepAliveTimeout = 120000; // 120 seconds
+server.headersTimeout = 120000;
